4030b2c556ea4f52325cff0c309dfef96a169656
h0ney <H0neyCatCoffeeGo@proton.me>
2024-08-17 08:37:37 +0500

    Automated commit message

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
-#include <iostream>
-
-#include "../include/raylib.h"
+#include "./include/raylib.h"
+#include "./include/Animation.h"
 
 int main (int argc, char *argv[]) {
   
-  std::cout << "Hello World" << std::endl;
+  SpriteAnimation animation = SpriteAnimation( "./assets/player.png", 3 );
+
+  animation.AddAnimation( "walk" , 0, 2); 
+
+  InitWindow( 64*5 , 64*5, "Animation Test");
+
+  animation.Init();
+
+  while ( !WindowShouldClose() )
+  { 
+
+    BeginDrawing();
+
+    animation.PlayAnimation( 64 * 3.5 , 64 * 3.5 , "walk" );
+
+    ClearBackground(GRAY);
+
+    EndDrawing();
+    
+  }
+
+  CloseWindow();
 
   return 0;
 }

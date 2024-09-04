d41be99f0dcd72ffb5fed2f8b01eeda07f66f3cb
CoffeeCup19 <h0neycatcoffeego@proton.me>
2024-09-04 11:34:56 +0500

    fixed

--- main.cpp
+++ main.cpp
@@ -1,22 +1,13 @@
 #include "./include/raylib.h"
-#include "./include/Animation.h"
 
 int main (int argc, char *argv[]) {
   
-  SpriteAnimation animation = SpriteAnimation( "./assets/player.png", 3 );
-
-  animation.AddAnimation( "walk" , 0, 2); 
-
   InitWindow( 64*5 , 64*5, "Animation Test");
-
-  animation.Init();
 
   while ( !WindowShouldClose() )
   { 
 
     BeginDrawing();
-
-    animation.PlayAnimation( 64 * 3.5 , 64 * 3.5 , "walk" );
 
     ClearBackground(GRAY);
 
